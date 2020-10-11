d14c36789efa67cdfb10e2ac4312b6f8062318d3
Mucahid Yazar <mucahidyazar@gmail.com>
2020-10-11 14:26:03 +0300

    Fixed a type bug

--- views/components/Boilerplate/index.tsx
+++ views/components/Boilerplate/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles.module.scss'
-import SearchSvg from '../../../public/static/assets/svgs/search.svg'
+//import SearchSvg from '../../../public/static/assets/svgs/search.svg'
 
 const BoilerPlate = () => {
   return (
